bad076d6c901556bbf0b0d5d87c2b46778bb294c
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2018-02-19 09:21:00 +0700

    Gilang: Add Unit Test

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -3,43 +3,50 @@ import * as actions from './index'
 
 describe('todo actions', () => {
   it('addTodo should create ADD_TODO action', () => {
-    expect(actions.addTodo('Use Redux')).toEqual({
+    expect(actions.addTodo('Use Redux')).to.deep.equal({
       type: types.ADD_TODO,
       text: 'Use Redux'
     })
   })
 
-  it('deleteTodo should create DELETE_TODO action', () => {
-    expect(actions.deleteTodo(1)).toEqual({
-      type: types.DELETE_TODO,
+  // it('deleteTodo should create DELETE_TODO action', () => {
+  //   expect(actions.deleteTodo(1)).toEqual({
+  //     type: types.DELETE_TODO,
+  //     id: 1
+  //   })
+  // })
+
+  // it('editTodo should create EDIT_TODO action', () => {
+  //   expect(actions.editTodo(1, 'Use Redux everywhere')).toEqual({
+  //     type: types.EDIT_TODO,
+  //     id: 1,
+  //     text: 'Use Redux everywhere'
+  //   })
+  // })
+
+  it('toggleTodo should create TOGGLE_TODO action', () => {
+    expect(actions.toggleTodo(1)).to.deep.equal({
+      type: types.TOGGLE_TODO,
       id: 1
     })
   })
 
-  it('editTodo should create EDIT_TODO action', () => {
-    expect(actions.editTodo(1, 'Use Redux everywhere')).toEqual({
-      type: types.EDIT_TODO,
-      id: 1,
-      text: 'Use Redux everywhere'
-    })
-  })
+  // it('completeAll should create COMPLETE_ALL action', () => {
+  //   expect(actions.completeAll()).toEqual({
+  //     type: types.COMPLETE_ALL
+  //   })
+  // })
 
-  it('completeTodo should create COMPLETE_TODO action', () => {
-    expect(actions.completeTodo(1)).toEqual({
-      type: types.COMPLETE_TODO,
-      id: 1
-    })
-  })
+  // it('clearCompleted should create CLEAR_COMPLETED action', () => {
+  //   expect(actions.clearCompleted()).toEqual({
+  //     type: types.CLEAR_COMPLETED
+  //   })
+  // })
 
-  it('completeAll should create COMPLETE_ALL action', () => {
-    expect(actions.completeAll()).toEqual({
-      type: types.COMPLETE_ALL
-    })
-  })
-
-  it('clearCompleted should create CLEAR_COMPLETED action', () => {
-    expect(actions.clearCompleted()).toEqual({
-      type: types.CLEAR_COMPLETED
+  it('setStateFilter should set FILTER action', () => {
+    expect(actions.setStateFilter(types.TODO_ALL)).to.deep.equal({
+      type: types.FILTER_TODO_BY_STATE,
+	    filter: types.TODO_ALL
     })
   })
 })
--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,4 +1,4 @@
-import SearchBar from "../SearchBar";
+import SearchBar from "../components/SearchBar";
 import Sinon from "sinon";
 
 describe("<SearchBar />", () => {
